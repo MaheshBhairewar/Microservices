eb0d143530f79816048c2182f42b58f759491bc1
Mahesh <mahehbhairewar@gmail.com>
2024-05-27 23:00:19 +0530

    Swagger API Documenation

--- accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -4,8 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(info = @Info(title = "Account Microservice REST API Documentation", description = "EasyBank Accout Microservice REST API Documentation", version = "v1", contact = @Contact(name = "Mahesh Bhairewar", email = "maheshbhairewar@gmail.com", url = "mahesh@portfolio;.com"), license = @License(name = "Apache 2.0", url = "mahesh@portfolio.com")), externalDocs = @ExternalDocumentation(description = "EasyBank Accounts Microservice REST API Documentation", url = "mahesh@portfolio.com"))
+
 public class AccountsApplication {
 
 	public static void main(String[] args) {
--- accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
@@ -1,20 +1,25 @@
 package com.eazybytes.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Schema(name = "Accounts", description = "Schema to hold Account information")
 public class AccountsDto {
-	
-	@NotEmpty(message= " AccountNumber can not be null or empty")
-	@Pattern(regexp = "(^$|[0-9]{10})",message="Account Number must be 10 digits")
+
+	@Schema(description = "Account Number of Eazy Bank account", example = "3454433243")
+	@NotEmpty(message = " AccountNumber can not be null or empty")
+	@Pattern(regexp = "(^$|[0-9]{10})", message = "Account Number must be 10 digits")
 	private Long accountNumber;
-	
-	@NotEmpty(message= " AccountType can not be null or empty")
+
+	@Schema(description = "Account type of Eazy Bank account", example = "Savings")
+	@NotEmpty(message = " AccountType can not be null or empty")
 	private String accountType;
-	
+
+	@Schema(description = "Eazy Bank branch address", example = "123 NewYork")
 	@NotEmpty(message = "BranchAddress can not be a null or empty")
 	private String branchAddress;
-	
+
 }
